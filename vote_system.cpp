2bab9533fdfe22e06ce1f5e3e263005716d752bf
behnoosh_bhy2002 <h.n.n.b1397@gmail.com>
2021-10-05 18:32:35 +0330

    forth commit _ gam 5

--- vote_system.cpp
+++ vote_system.cpp
@@ -4,6 +4,9 @@
 #include<cstring>
 #include<fstream>
 #include<sstream>
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
 using namespace std;
 //class of movies information
 class movie
@@ -24,55 +27,19 @@ class movie
        void set_title(string subject){ title = subject; }
        void set_region(string place){ region = place; }
        void set_language(string lan){ language = lan; }
-       void set_types(string arr){ types = arr;
-
-           /*
-                      int j=0;
-           for(auto i : arr){
-            types[j]=i;
-            j++;
-           }
-           ////////////////////////
-             do{
-                types[j]=arr[j];
-                 j++;
-             }while(arr[j]!=nullptr);
-           */
-       }
-       void set_attributes(string att){ attributes = att;
-           /*
-                      int j=0;
-           for(auto i : att){
-            attributes[j]=i;
-            j++;
-           }
-           //////////////////////////
-             do{
-                 attributes[j]=att[j];
-                 j++;
-             }while(att[j]!=nullptr);
-           */
-       }
+       void set_types(string arr){ types = arr;}
+       void set_attributes(string att){ attributes = att;}
        void set_isOrigin(bool makeSure){ isOrginalTitle = makeSure; }
+
        //getter function
        string get_titleID(){ return titleID; }
        int get_ordring(){ return ordering; }
        string get_title(){ return title; }
        string get_region(){ return region; }
        string get_language(){ return language; }
-       string get_types(){
-         /*  for(auto i : types){
-            return i;
-           } */
-           return types;
-       }
-       string get_attributes(){
-          /* for(auto i : attributes){
-            return i;
-           } */
-           return attributes;
-       }
-       bool get_isOrigin(){ return isOrginalTitle;}
+       string get_types(){ return types; }
+       string get_attributes(){ return attributes; }
+       bool get_isOrigin(){ return isOrginalTitle; }
 
 
 };
@@ -114,6 +81,7 @@ class user
         {
             return list_movie_vote;
         }
+        //checking input username
         bool check_user(string users , user person[] , int num)
         {
             for(int i=0; i<num; i++){
@@ -123,6 +91,7 @@ class user
             }
             return false;
         }
+        //checking input password
         bool check_pass(string users , string pass , user person[] , int num){
             for(int i=0; i<num; i++){
                 if(users == person[i].get_username() ){
@@ -133,6 +102,7 @@ class user
                 }
             }
         }
+        //checking delicated votes
         bool check_vote( string id  ){
              int len = sizeof(list_movie_vote) / sizeof(list_movie_vote[0]);
 
@@ -144,70 +114,205 @@ class user
         }
 
 };
-void menu(user person[] , movie obj[] , voteRate rate[] , int shomare , int num_movie)
+//chicking ides and returning name of movie
+string findName(string id , movie obj[] , int num)
 {
-    int choise;
-    bool flag=1;
+    for(int i=1; i<num; i++){
+        if(obj[i].get_titleID()==id)
+            return obj[i].get_title();
+    }
+}
+//showing movies with information
+void showTable(movie obj[] , voteRate rate[] , int num_movie)
+{
+    printf("%-10s | %-10s | %-15s | %-10s | %-10s | %-10s | %-10s | %-5s | %-5s | \n" ,"titleId" ,"ordering" , "title"  , "region" ,"Rating",
+            "language" , "types" ,"attributes" , "isOriginalTitle" );
+    cout<<"---------------------------------------------------------------------------------------------------------------------------\n\n";
+        for(int i=1; i<num_movie; i++){
+
+            printf("%-10s | %-10d | %-15s | %-10s | %-10.1f | " , obj[i].get_titleID().c_str()  ,
+                                            obj[i].get_ordring() , obj[i].get_title().c_str() , obj[i].get_region().c_str() , floor( rate[i].get_averageRating() * 10 + .5 ) / 10 );
+            printf( "%-10s | %-10s | %-5s | %-5d | \n\n" , obj[i].get_language().c_str() ,
+                                            obj[i].get_types().c_str() , obj[i].get_attributes().c_str() ,obj[i].get_isOrigin());
+            cout<<"---------------------------------------------------------------------------------------------------------------------------\n\n";
+        }
+}
+//vote part
+void vote_movie(user person[] , voteRate rate[] , int num_movie , int shomare){
     string id;
     float rating;
-    while(flag==1){
-        cout<<"1.Show movie's list\n";
-        cout<<"2.vote movie\n";
-        cout<<"3.Top Ten\n";
-        cout<<"4.Voted videos\n";
-        cout<<"5.Exit\n";
-        cin>>choise;
-         switch(choise){
-              case 1:
-                   cout<<"titleId\tordering\ttitle\tregion\tlanguage\ttypes\tattributes\tisOriginalTitle\n";
-                 for(int i=1; i<num_movie; i++){
-                   cout<<obj[i].get_titleID()<<"\t"<<obj[i].get_ordring()<<"\t";
-                   cout<<obj[i].get_title()<<"\t"<<obj[i].get_region()<<"\t";
-                   cout<<obj[i].get_language()<<"\t"<<obj[i].get_types()<<"\t";
-                   cout<<obj[i].get_attributes()<<"\t"<<obj[i].get_isOrigin()<<endl;
-                   cout<<"------------------------------------------------------------------------------------------------------\n";
-                 }
-                 break;
-              case 2:
-
-
-                 while(1){
-                      cout<<"Enter title id of movie : ";
-                      cin>>id;
-                      if( person[shomare].check_vote( id ))
-                        cout<<"You have voted to this movie before please choose another one\n ";
-                      else
-                        break;
-                   }
-                 cout<<"Enter your rating : ";
-                   do{
-                     cin>>rating;
+    while(1){
+            cout<<"Enter title id of movie : ";
+            cin>>id;
+                if( person[shomare].check_vote( id ))
+                    cout<<"You have voted to this movie before please choose another one\n ";
+                else
+                    break;
+                }
+            cout<<"Enter your rating : ";
+                do{
+                    cin>>rating;
                        if(rating < 0 || rating > 10)
-                         cout<<"Invalid rating try again\nEnter your rating : ";
-                   }while(rating < 0 || rating > 10);
+                          cout<<"Invalid rating try again\nEnter your rating : ";
+                }while(rating < 0 || rating > 10);
 
-                   for(int i=0; i<num_movie; i++){
-                      if(rate[i].get_tconst()==id){
+                for(int i=0; i<num_movie; i++){
+                    if(rate[i].get_tconst()==id){
                         float r = ( ( rate[i].get_averageRating()*rate[i].get_numVote())+rating ) / (rate[i].get_numVote()+1);
+                        r = floor ( r * 10 + .5 ) / 10;
                         rate[i].set_averageRating(r);
                         rate[i].set_numVote(rate[i].get_numVote() + 1);
                         cout<<(rate[i].get_averageRating())<<endl<<(rate[i].get_numVote())<<endl;
-                      }
-                   }
+                    }
+                }
                    person[shomare].set_listOfMovie( id);
+}
+//sort and print TopTen Movies
+void Sort(voteRate rate[] , movie obj[] , int num_movie)
+{
+     voteRate temp[45];
+     string id;
+     float av_rating;
+     int num;
+     //copy rating 
+     for(int k=0; k<num_movie; k++){
+        temp[k].set_tconst(rate[k].get_tconst());
+        temp[k].set_averageRating(rate[k].get_averageRating());
+        temp[k].set_numVote(rate[k].get_numVote());
+     }
+    //sort movies by rating
+     for(int k=0; k<num_movie; k++){
+        for(int j=k+1; j<num_movie; j++){
+            if(temp[j].get_averageRating() > temp[k].get_averageRating()){
+
+                id = temp[j].get_tconst();
+                av_rating = temp[j].get_averageRating();
+                num = temp[j].get_numVote();
+
+                temp[j].set_tconst(temp[k].get_tconst());
+                temp[j].set_averageRating(temp[k].get_averageRating());
+                temp[j].set_numVote(temp[k].get_numVote());
+
+                temp[k].set_tconst(id);
+                temp[k].set_averageRating(av_rating);
+                temp[k].set_numVote(num);
+
+                }
+          }
+     }
+     //printig TopTen
+     printf(" %-50s | %-10.1s|\n" , "Title" , "Rating");
+      cout<<"------------------------------------------------------------------\n";
+    for(int k=0; k<10; k++){
+        printf(" %-50s | %-10.1f |\n" , findName(temp[k].get_tconst() , obj , num_movie).c_str() , temp[k].get_averageRating());
+       cout<<"------------------------------------------------------------------\n";
+        }
+}
+//Menu
+void menu(user person[] , movie obj[] , voteRate rate[] , int shomare , int num_movie)
+{
+    int choise;
+    bool flag=true;
+
+    while(flag==true){
+        cout<<"1.Show movie's list\n";
+        cout<<"2.vote movie\n";
+        cout<<"3.Top Ten\n";
+        cout<<"4.Exit\n";
+        cin>>choise;
+         switch(choise){
+              case 1:
+                 showTable( obj , rate , num_movie );
+                 break;
+              case 2:
+                 vote_movie( person , rate , num_movie , shomare );
                  break;
               case 3:
+                 Sort( rate , obj , num_movie );
                  break;
-              case 4:
-                 break;
-              case 5:
-                 flag=0;
-                 break;
-              default:
+               case 4:
+                 flag=false;
                  break;
 
          }
     }
+}
+//User
+void UserPart(movie obj[] , voteRate Rate[] , int counter){
+
+
+    cout<<"**   **  **** **    **    **  ****   **      ***   *****    **     **  **** \n";
+    cout<<"**   **   **   **   **   **   **     **    **     *     *   ** * * **  **\n";
+    cout<<"*******   **    **  **  **    ***    **    **     *     *   **  *  **  ***\n";
+    cout<<"**   **   **     ** ** **     **     **    **     *     *   **     **  **\n";
+    cout<<"**   **  ****     **  **      ****   *****   ***   *****    **     **  ****\n";
+
+     int choose , shomare=0;
+     string username , password;
+     user person[100];
+     while(1){
+
+        cout<<"1.Login \n2.Sign Up\n3.Exit \n";
+        cin>>choose;
+        if(choose==1){
+            cout<<"username : ";
+            cin>>username;
+            cout<<"password : ";
+            cin>>password;
+               //checking username
+               if(person[shomare].check_user(username , person, shomare))
+                {
+
+                  while(1)
+                  {
+                     //checking password
+                     if(person[shomare].check_pass(username , password  , person , shomare ))
+                     {
+
+                           menu(person , obj , Rate , shomare , counter);
+                           break;
+                     }
+                     else{
+                        cout<<"invalid password try again \n";
+                        cout<<"password : ";
+                        cin>>password;
+                     }
+
+                 }
+               }
+               else{
+                  cout<<"Incorrect username \nplease sign up  first!! \n";
+               }
+           }
+        else if(choose==2){
+
+              while(1){
+               cout<<"username : ";
+               cin>>username;
+                 //checking for duplicate username
+                 if(person[shomare].check_user(username , person, shomare)){
+                    cout<<"This is duplicate username try sth else \n";
+                 }
+                 else{
+                    cout<<"password : ";
+                    cin>>password;
+                    person[shomare].set_username(username);
+                    person[shomare].set_password(password);
+                    cout<<"You signed up successfully!! \nYou can login now\n";
+                    shomare++;
+                    break;
+                 }
+              }
+        }
+        else if(choose==3){
+             break;
+        }
+        else{
+           cout<<"Incorrect value please try again \n";
+        }
+       // system("cls");
+     }
+
 }
 int main()
 {
@@ -229,49 +334,47 @@ int main()
      while (getline(file_movie, line))
     {
 
-    std::stringstream  linestream(line);
-    getline(linestream, TI, '\t');
-    getline(linestream ,ord, '\t');
-    getline(linestream, sub, '\t');
-    getline(linestream, place, '\t');
-    getline(linestream, lan, '\t');
-    getline(linestream, kind, '\t');
-    getline(linestream, att , '\t');
-    getline(linestream, isOrginal, '\t');
-    //setting data to class object
-      if(counter!=0){
-         obj[counter].set_titleID(TI);
-         //converting string to integer
-         stringstream ss;
-         ss << ord;
-         ss >> order;
-         obj[counter].set_ordring(order);
-         obj[counter].set_title(sub);
-         obj[counter].set_region(place);
-         obj[counter].set_language(lan);
-         obj[counter].set_types(kind);
-         obj[counter].set_attributes(att);
-         //converting string to boolean
-          if(isOrginal=="1")
-            makeSure = true;
-          else
-            makeSure = false;
-         obj[counter].set_isOrigin(makeSure);
+      std::stringstream  linestream(line);
+      getline(linestream, TI, '\t');
+      getline(linestream ,ord, '\t');
+      getline(linestream, sub, '\t');
+      getline(linestream, place, '\t');
+      getline(linestream, lan, '\t');
+      getline(linestream, kind, '\t');
+      getline(linestream, att , '\t');
+      getline(linestream, isOrginal, '\t');
+      //setting data to class object
+        if(counter!=0){
+          obj[counter].set_titleID(TI);
+          //converting string to integer
+          stringstream ss;
+          ss << ord;
+          ss >> order;
+          obj[counter].set_ordring(order);
+          obj[counter].set_title(sub);
+          obj[counter].set_region(place);
+          obj[counter].set_language(lan);
+          obj[counter].set_types(kind);
+          obj[counter].set_attributes(att);
+          //converting string to boolean
+           if(isOrginal=="1")
+             makeSure = true;
+           else
+             makeSure = false;
+          obj[counter].set_isOrigin(makeSure);
        }
 
-
-    //cout<<counter<<endl;
-   //  cout<<obj[counter].get_titleID()<<"\t"<<ord<<"\t"<<sub<<"\t"<<place<<"\t"<<lan<<"\t"<<kind<<"\t"<<att<<"\t"<<isOrginal<<endl;
       counter++;
     }
+
      file_movie.close();
     // opening rating file
     ifstream file_rating;
     file_rating.open("title.ratings.txt");
-    if(!file_rating){
-        cout<<"error when opening file\n";
-        exit(1);
-     }
+      if(!file_rating){
+         cout<<"error when opening file\n";
+         exit(1);
+      }
      string num , average;
      int c=0;
      while (getline(file_rating, line)){
@@ -289,86 +392,25 @@ int main()
              str << num;
              str >> numVote;
              Rate[c].set_numVote(numVote);
-           //  cout<<Rate[c].get_tconst()<<"\t"<<Rate[c].get_averageRating()<<"\t"<<Rate[c].get_numVote()<<endl;
+             //  cout<<Rate[c].get_tconst()<<"\t"<<Rate[c].get_averageRating()<<"\t"<<Rate[c].get_numVote()<<endl;
             }
 
             c++;
      }
      file_rating.close();
 
-
-     ///////////////////////////////////////////
-     //user part
-     int choose , shomare=0;
-     string username , password;
-     user person[100];
-     while(1){
-
-        cout<<"1.login \n2.sign up\n";
-        cin>>choose;
-        if(choose==1){
-            cout<<"username : ";
-            cin>>username;
-            cout<<"password : ";
-            cin>>password;
-               if(person[shomare].check_user(username , person, shomare))
-                {
-
-                  while(1)
-                  {
-
-                     if(person[shomare].check_pass(username , password  , person , shomare ))
-                     {
-
-                           menu(person , obj , Rate , shomare , counter);
-                           break;
-                     }
-                     else{
-                        cout<<"invalid password try again \n";
-                        cout<<"password : ";
-                        cin>>password;
-                     }
-
-                 }
-                // for(int c=1; c<45; c++)
-                // cout<<Rate[c].get_tconst()<<"\t"<<Rate[c].get_averageRating()<<"\t"<<Rate[c].get_numVote()<<endl;
-               }
-               else{
-                  cout<<"Incorrect username \nplease sign up  first!! \n";
-               }
-        }
-        else if(choose==2){
-
-              while(1){
-               cout<<"username : ";
-               cin>>username;
-                 if(person[shomare].check_user(username , person, shomare)){
-                    cout<<"This is duplicate username try sth else \n";
-                 }
-                 else{
-                    cout<<"password : ";
-                    cin>>password;
-                    person[shomare].set_username(username);
-                    person[shomare].set_password(password);
-                    cout<<"You signed up successfully!! \nYou can login now\n";
-                    shomare++;
-                    break;
-                 }
-              }
-        }
-        else{
-             cout<<"Incorrect value please try again \n";
-        }
-     }
+     UserPart(obj , Rate , counter);
+     ofstream R("title.ratings.txt");
+       if(R.is_open())
+       {
+           R<<"tconst\taverageRating\tnumVotes\n";
+             for(int i=1; i<counter; i++)
+             {
+                 R<<Rate[i].get_tconst()<<"\t";
+                 R<<Rate[i].get_averageRating()<<"\t";
+                 R<<Rate[i].get_numVote()<<"\n";
+             }
+             R.close();
+       }
 
 }
-
-
-
-
-
-
-
-
-
-
